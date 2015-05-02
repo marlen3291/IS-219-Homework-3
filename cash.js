5479efb6f642fd68d00848340480deffe61b5561
Mar Leonard Lopez <marlen3291@gmail.com>
2015-05-02 16:57:18 -0400

    I did the additional bonus testing with istanbul and travis

--- cash.js
+++ cash.js
@@ -1,4 +1,4 @@
-var C = {};                    // C Object simplifies exporting the module
+
 var C = {};     // C Object simplifies exporting the module
 C.coins = [5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
 C.getChange = function (totalPayable, cashPaid) {
